bc3569675e73452dbe188292adc776442cfd2eaf
Matheus Hendrico <69541672+Hendricoo@users.noreply.github.com>
2023-06-06 10:39:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,36 @@
 import streamlit as st
-import cv2
-import numpy as np
+from json import loads
+from pandas import read_csv
 
-def remove_background(image):
-    # Converta a imagem para escala de cinza
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Aplique uma transformação para remover o fundo
-    _, mask = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY_INV)
-    
-    # Aplique a máscara na imagem original
-    result = cv2.bitwise_and(image, image, mask=mask)
-    
-    return result
+st.markdown('''
+# Exibidor de arquivos
 
-def main():
-    st.title("Removedor de Background")
-    
-    # Carregue a imagem
-    uploaded_file = st.file_uploader("Selecione uma imagem", type=["jpg", "jpeg", "png"])
-    
-    if uploaded_file is not None:
-        # Leia a imagem
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        image = cv2.imdecode(file_bytes, 1)
-        
-        # Redimensione a imagem para caber na tela
-        max_width = 800
-        if image.shape[1] > max_width:
-            scale_factor = max_width / image.shape[1]
-            image = cv2.resize(image, (max_width, int(image.shape[0] * scale_factor)))
-        
-        st.image(image, channels="BGR", caption="Imagem Original")
-        
-        # Remova o fundo da imagem
-        result = remove_background(image)
-        
-        st.image(result, channels="BGR", caption="Imagem sem Background")
+## Suba um arquivo e vejamos o que acontece :smile::heart:
+''')
 
-if __name__ == "__main__":
-    main()
+
+arquivo = st.file_uploader(
+    'Suba seu arquivo aqui!',
+    type=['jpg', 'png', 'py', 'wav', 'csv', 'json']
+)
+
+st.text_input('Email', max_chars=10)
+st.text_input('Senha', type='password')
+
+if arquivo:
+    print(arquivo.type)
+    match arquivo.type.split('/'):
+        case 'application', 'json':
+            st.json(loads(arquivo.read()))
+        case 'image', _:
+            st.image(arquivo)
+        case 'text', 'csv':
+            df = read_csv(arquivo).transpose()
+            st.dataframe(df)
+            st.bar_chart(df)
+        case 'text', 'x-python':
+            st.code(arquivo.read().decode())
+        case 'audio', _:
+            st.audio(arquivo)
+else:
+    st.error('Ainda não tenho arquivo!')
